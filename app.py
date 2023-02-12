82808cec8dbfa6e69135b3b6c5cde8f83f3367dc
Anant Tyagi <anant21100008@akgec.ac.in>
2023-02-12 13:01:41 +0530

    API

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ CORS(app)
 def recommend_movies():
     res,mes=course.recomended_course(request.args.get('title'))
     
-    columns=[{'course0':res[0], 'link0':mes[0]},{'course1':res[1], 'link1':mes[1]},{'course2':res[2], 'link2':mes[2]},{'course3':res[3], 'link3':mes[3]},
-    {'course4':res[4], 'link4':mes[4]}]
+    columns=[{'course':res[0], 'link':mes[0]},{'course':res[1], 'link':mes[1]},{'course':res[2], 'link':mes[2]},{'course':res[3], 'link':mes[3]},
+    {'course':res[4], 'link':mes[4]}]
 
     return jsonify(columns)
 
